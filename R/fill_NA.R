1ce3d1a21c2aa85d209afcbadb2204ca94f56ea9
mosleyl <62598202+mosleyl@users.noreply.github.com>
2023-01-10 14:44:10 +0000

    Add files via upload

---
+++ R/fill_NA.R
@@ -0,0 +1,41 @@
+## data set with missing values replaced with cubic spline (between observed values) and 
+## 1-D digital filter fitted values for missing tails calculated at the variable level.
+## This is partly taken from nowcasting R package 
+
+## Interpolation of missing data for initPCA
+##
+## Internal missing data is filled in using a cubic spline. 
+## Start and end of sample missing data is filled in using the median of the series and then
+## smoothed with a MA(3) process. 
+
+
+fill_NA <-function(X){
+  
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  k <- 3
+  idx.na <- is.na(X)
+  
+  for (i in 1:p){  
+    x = X[,i]
+    na_x = is.na(x)
+    t1 = min(which(!na_x))
+    t2 = max(which(!na_x))
+    
+    x1 <- stats::spline(x[t1:t2],xout = 1:(t2-t1+1))
+    xx <- x1$y
+    x[t1:t2] <- x1$y
+    na_x <- is.na(x)
+    x[na_x] <- median(x,na.rm = T)
+    
+    x_MA3 <- stats::filter(x = c(rep(x[1],k),x,rep(x[length(x)],k)),filter = rep(1,2*k+1)/(2*k+1),sides = 1)
+    
+    x_MA3 = x_MA3[(2*k+1):length(x_MA3)]
+    x[idx.na[,i]] = x_MA3[idx.na[,i]]
+    X[,i] = x
+  }
+  
+  
+  return(list(X = X, idx.na=idx.na))
+  
+}
